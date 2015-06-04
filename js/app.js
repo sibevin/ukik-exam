917ad339c3919cca95b59ec4d6190900fbaa87fc
Sibevin Wang <sibevin@gmail.com>
2015-06-04 12:08:32 +0800

    Upload answer-03
    DESC: F

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(function(){
-  $("#menu_switch").on("click", function(){
+  var display_menu = function(){
     if ($("#menu").is(":visible")) {
       $("#menu").hide();
+      $("#menu_background").hide();
     } else {
       $("#menu").show();
+      $("#menu_background").show();
     }
-  });
+  };
+  $("#menu_switch").on("click", display_menu);
+  $("#menu_background").on("click", display_menu);
 });
